851dab5e24aa20ff7774e9d1cc424241d4544f3d
EdouardBke <Edouardmakenga0@gmail.com>
2021-01-06 19:45:28 +0100

    Creation db and migration problem

--- ExamManager/app/__init__.py
+++ ExamManager/app/__init__.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db = SQLAlchemy(app=app)
 
-from app import models, views
+from app import default,views,db_create,db_migrate,db_upgrade,db_downgrade
---
+++ ExamManager/app/default.py
@@ -0,0 +1,57 @@
+from app import models
+import datetime
+
+
+def run():
+    # examiners = [Examiner(2021001, "Chloe Vann der Sar", "chloe.vann.der.sar@gmail.com"),
+    #              Examiner(2021002, "vassita volgov ",
+    #                       "vassitavolgov1@gmail.com"),
+    #              Examiner(2021003, "Aleksey Volkov",
+    #                       "AlekseyVolkov@gmail.com"),
+    #              Examiner(2021004, "Mounim Mouani", "Mounimmouani@gmail.com"),
+    #              Examiner(2021005, "Monsif Chouini",
+    #                       "Monsif Chouini@gmail.com"),
+    #              Examiner(2021006, "Edward McBell", "EdwardMcBell@gmail.com"),
+    #              Examiner(2021007, "Paula Kimberly",
+    #                       "PaulaKimberly@gmail.com"),
+    #              Examiner(2021008, "Bill Kim", "BillKim@gmail.com"),
+    #              Examiner(2021009, "Wiiliam GeorgeTown",
+    #                       "WiiliamGeorgeTown@gmail.com"),
+    #              Examiner(2021010, "Michael Scofield",
+    #                       "Michael Scofield@gmail.com"),
+    #              Examiner(20210011, "Lincoln Burrows",
+    #                       "Lincoln Burrows@gmail.com"),
+    #              Examiner(20210012, "Aleksander Mahone",
+    #                       "AleksanderMahone@gmail.com"),
+    #              Examiner(20210013, "Paul Killerman",
+    #                       "PaulKillerman@gmail.com"),
+    #              Examiner(20210014, "David Ballern", "DavidBallern@gmail.com"),
+    #              Examiner(20210015, "Sergio Suarez", "SergioSuarez@gmail.com")]
+
+    # students = [Student(251567, "Edouard Makenga Badibake", "edouardmkakenga0@gmail.com"),
+    #             Student(251452, "Maxime LeBlanc", "MaximeLeBlanc@gmail.com"),
+    #             Student(251230, "Peter LeRusse", "PeterLeRusse0@gmail.com"),
+    #             Student(251231, "Avril Lavigne", "AvrilLavigne0@gmail.com"),
+    #             Student(251232, "Marvin Mcfadden",
+    #                     "MarvinMcfadden0@gmail.com"),
+    #             Student(251233, "Natasha Kooper", "NatashaKooper@gmail.com"),
+    #             Student(251234, "Ed Sheeran", "EdSheeran@gmail.com"),
+    #             Student(251235, "Bob Marley", "BobMarley@gmail.com"),
+    #             Student(251236, "Piotr Fabianski", "PiotrFabianski@gmail.com"),
+    #             Student(251237, "Dead Poll", "DeadPoll@gmail.com"),
+    #             Student(251238, "Luna Tuna", "LunaTuna@gmail.com"),
+    #             Student(251239, "Carlos Moreli", "CarlosMoreli.com"),
+    #             Student(251455, "Xing zi ji ping", "Xingzijiping@gmail.com")]
+
+    # exams = [Exam(1202100001, datetime.date("2021-02-14"), 1),
+    #          Exam(1202100002, datetime.date("2021-03-14"), 2),
+    #          Exam(1202100003, datetime.date("2021-01-14"), 3),
+    #          Exam(1202100004, datetime.date("2021-07-14"), 5),
+    #          Exam(1202100005, datetime.date("2021-08-14"), 4),
+    #          Exam(1202100006, datetime.date("2021-08-14"), 6)]
+    # for exam in exams:
+    #     print(exam.exam_id)
+    pass
+
+
+run()
